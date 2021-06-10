9def5dad7f95427d7b5b72906ba1349a48bdc61d
izzzzzzi <noeymaiih@gmail.com>
2021-06-11 00:29:55 +0900

    modify NotFound.vue

--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -1,12 +1,13 @@
 <template>
   <v-main>
     <v-container>
-      <v-card class="mx-auto card-not-found" max-width="500" height="500">
-        <v-card-title>Not Found</v-card-title>
+      <!-- v-sheet는 Vuetify 기능의 기본 기반을 제공하는 변형 가능한 paper 컴포넌트 -->
+      <v-sheet class="mx-auto text-center " max-width="500" height="500">
+        <div class="grey--text title">Page Not Found</div>
         <v-btn text color="deep-purple accent-4" @click="backPage">
           뒤로 가기
         </v-btn>
-      </v-card>
+      </v-sheet>
     </v-container>
   </v-main>
 </template>
@@ -21,12 +22,4 @@ export default {
 };
 </script>
 
-<style>
-.card-not-found {
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-
-  align-items: center;
-}
-</style>
+<style></style>
