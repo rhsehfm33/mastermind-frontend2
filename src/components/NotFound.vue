29033e529890ffde8f479613461c87ce7b2016d7
izzzzzzi <noeymaiih@gmail.com>
2021-06-11 01:44:25 +0900

    modify notfound.vue

--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -1,15 +1,20 @@
 <template>
-  <v-main>
-    <v-container>
-      <!-- v-sheet는 Vuetify 기능의 기본 기반을 제공하는 변형 가능한 paper 컴포넌트 -->
+  <!-- <v-container>
       <v-sheet class="mx-auto text-center " max-width="500" height="500">
         <div class="grey--text title">Page Not Found</div>
         <v-btn text color="deep-purple accent-4" @click="backPage">
           뒤로 가기
         </v-btn>
       </v-sheet>
-    </v-container>
-  </v-main>
+    </v-container> -->
+  <v-container class="fill-height">
+    <v-row align="center" justify="center" class="flex-column">
+      <h1 class="grey--text title">PAGE NOT FOUND</h1>
+      <v-btn text color="deep-purple accent-4" @click="backPage">
+        뒤로 가기
+      </v-btn>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
