b6788122b5d8a22e0786a888810d4aeaf985d504
izzzzzzi <noeymaiih@gmail.com>
2021-06-12 01:19:00 +0900

    docs: Add comment to about register function

--- src/api/index.js
+++ src/api/index.js
@@ -1,91 +1,94 @@
-import axios from 'axios'
-import router from '../router'
+import axios from "axios";
+import router from "../router";
 
-const domain = 'http://localhost:3000'
-const Unauthorized = 401
+const domain = "http://localhost:3000";
+const Unauthorized = 401;
 const onUnauthorized = () => {
-  router.push(`/login?returnPath=${encodeURIComponent(location.pathname)}`)
-}
+  router.push(`/login?returnPath=${encodeURIComponent(location.pathname)}`);
+};
 
 const request = {
   get(path) {
-    return axios.get(`${domain + path}`)
-      .catch(({response}) => {
-        const {status} = response
-        if (status === Unauthorized) return onUnauthorized()
-        throw Error(response)
-      })
+    return axios.get(`${domain + path}`).catch(({ response }) => {
+      const { status } = response;
+      if (status === Unauthorized) return onUnauthorized();
+      throw Error(response);
+    });
   },
   post(path, data) {
-    return axios.post(`${domain + path}`, data)
+    return axios.post(`${domain + path}`, data);
   },
   delete(path) {
-    return axios.delete(`${domain + path}`)
+    return axios.delete(`${domain + path}`);
   },
   put(path, data) {
-    return axios.put(`${domain + path}`, data)
+    return axios.put(`${domain + path}`, data);
   }
-}
+};
 
 export const setAuthInHeader = token => {
-  axios.defaults.headers.common['Authorization'] = token ? `Bearer ${token}` : null;
-}
+  axios.defaults.headers.common["Authorization"] = token
+    ? `Bearer ${token}`
+    : null;
+};
 
 export const auth = {
   login(email, password) {
-    return request.post('/login', {email, password})
-      .then(({data}) => data)
+    return request.post("/login", { email, password }).then(({ data }) => data);
   },
+  // 이메일 중복 체크
   check_email(email) {
-    return request.post('/check_email', {email})
-      .then(({data}) => data)
+    return request.post("/check_email", { email }).then(({ data }) => data);
   },
+  // 회원 가입
   register(name, email, password) {
-    return request.post('/register', {name, email, password})
+    return request.post("/register", { name, email, password });
   }
-}
+};
 
 export const board = {
   fetch(id) {
     if (id) {
-      return request.get(`/boards/${id}`).then(({ data }) => data)
+      return request.get(`/boards/${id}`).then(({ data }) => data);
     }
-    return request.get('/boards').then(({data}) => data)
+    return request.get("/boards").then(({ data }) => data);
   },
-  create (title) {
-    return request.post('/boards', { title }).then(({ data }) => data)
+  create(title) {
+    return request.post("/boards", { title }).then(({ data }) => data);
   },
   update(id, data) {
-    return request.put(`/boards/${id}`, data).then(({ data }) => data)
+    return request.put(`/boards/${id}`, data).then(({ data }) => data);
   },
   destroy(id) {
-    return request.delete(`/boards/${id}`)
+    return request.delete(`/boards/${id}`);
   }
-}
+};
 
 export const list = {
   create(data) {
-    return request.post(`/lists`, data)
+    return request.post(`/lists`, data);
   },
   update(id, data) {
-    return request.put(`/lists/${id}`, data).then(({ data }) => data)
+    return request.put(`/lists/${id}`, data).then(({ data }) => data);
   },
   destroy(id) {
-    return request.delete(`/lists/${id}`).then(({ data }) => data)
+    return request.delete(`/lists/${id}`).then(({ data }) => data);
   }
-}
+};
 
 export const card = {
   fetch(id) {
-    return request.get(`/cards/${id}`).then(({ data }) => data)
+    return request.get(`/cards/${id}`).then(({ data }) => data);
   },
-  create({title, listId, pos}) {
-    return request.post(`/cards`, {title, listId, pos}).then(({ data }) => data)
+  create({ title, listId, pos }) {
+    return request
+      .post(`/cards`, { title, listId, pos })
+      .then(({ data }) => data);
   },
   update(id, data) {
-    return request.put(`/cards/${id}`, data).then(({ data }) => data)
+    return request.put(`/cards/${id}`, data).then(({ data }) => data);
   },
   destroy(id) {
-    return request.delete(`/cards/${id}`)
+    return request.delete(`/cards/${id}`);
   }
-}
+};
